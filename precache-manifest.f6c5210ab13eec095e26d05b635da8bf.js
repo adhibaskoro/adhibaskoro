b8774b99b94b7e2300930176d22678dfb8a594cd
Adhi Baskoro <adhi.baskoro.95@gmail.com>
2020-10-17 11:28:27 +1100

    Updates

---
+++ precache-manifest.f6c5210ab13eec095e26d05b635da8bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "097bcabbcd110f07a7812342236d8161",
+    "url": "/adhibaskoro/index.html"
+  },
+  {
+    "revision": "6332d7b0ebec02a0ab2b",
+    "url": "/adhibaskoro/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c01ed24c54253f0e4b58",
+    "url": "/adhibaskoro/static/js/2.558f5ea4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/adhibaskoro/static/js/2.558f5ea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6332d7b0ebec02a0ab2b",
+    "url": "/adhibaskoro/static/js/main.19a614c1.chunk.js"
+  },
+  {
+    "revision": "427ec112a9d3476366b2",
+    "url": "/adhibaskoro/static/js/runtime-main.5993ec95.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/adhibaskoro/static/media/logo.5d5d9eef.svg"
+  }
+]);
